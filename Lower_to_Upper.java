62ac06ea14c23922c2baa735a237ed067d975b2a
mjh9401 <80654599+mjh9401@users.noreply.github.com>
2021-03-31 21:22:08 +0900

    Add files via upload
    From_Lower_String_to_Upper_String

---
+++ Lower_to_Upper.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Lower_to_Upper {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		String lower = sc.next();
+		System.out.println(lower.toUpperCase()); // toUpperCase()-> 소문자를 대문자로 변환
+
+	}
+
+}
